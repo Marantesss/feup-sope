ed1f78fcce26fb0ae0a8edd30724d2484cd83bb0
Marantesss <up201706917@fe.up.pt>
2019-02-20 19:07:04 +0000

    In the middle of exercise sheet 1

--- TP1/p5.c
+++ TP1/p5.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 // also works int main(int argc, char **argv, char **envp) {
 int main(int argc , char* argv[], char* envp[]) {
 
+    // a) & b)
     char user[30];
-
     for (int i = 0; envp[i] != NULL; i++) {
         // to print all environment variables
         // printf("%s\n", envp[i]);
@@ -19,11 +20,23 @@ int main(int argc , char* argv[], char* envp[]) {
         printf("%s\n", *env);  
     }
     */
-   
+
     char username[25];
     strcpy(username, user + 5);
-
     printf("Hello %s\n", username);
 
+    // c)
+    printf("PATH: %s\n", getenv("PATH"));
+    printf("HOME: %s\n", getenv("HOME"));
+    printf("USER: %s\n", getenv("USER"));
+    printf("ROOT: %s\n", getenv("ROOT"));
+    printf("Hello again %s, this time using getenv :)\n", getenv("USER"));
+
+    // d)
+    // we need to export USER_NAME="INSERT NAME HERE" -> You can use spaces as ling as you write it between ""
+    printf("USER_NAME: %s\n", getenv("USER_NAME"));
+
+    printf("Hello there General %s\n", getenv("USER_NAME"));
+    
     return 0;
 }
